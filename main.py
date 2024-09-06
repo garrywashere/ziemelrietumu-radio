88eed28c45aff25bfc801279b226ea73869c0887
Garry <github@garrynet.co.uk>
2024-09-06 15:42:17 +0100

    Removed logging for now
     - Issues with mounting the log file would crash the server
     - I could fix it with an exception handler but I want to overhaul the
       logging system, if not entirely remove it
      - It is a little pointless for now

--- main.py
+++ main.py
@@ -6,45 +6,45 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 
-def log_info():
-    time = datetime.now().strftime("%d/%m/%y @ %H:%M:%S")
-    real_ip = request.headers.get("X-Real-IP")
-    url = request.url
-    user_agent = request.headers.get("User-Agent")
+# def log_info():
+#     time = datetime.now().strftime("%d/%m/%y @ %H:%M:%S")
+#     real_ip = request.headers.get("X-Real-IP")
+#     url = request.url
+#     user_agent = request.headers.get("User-Agent")
 
-    log = (
-        "=" * 10,
-        " GET REQUEST ",
-        "=" * 10,
-        f"\nTime: {time}",
-        f"\nIP: {real_ip}",
-        f"\nURL: {url}",
-        f"\nAgent: {user_agent}\n",
-        "=" * 10 + " ",
-        "-" * 11 + " ",
-        "=" * 10,
-        "\n" * 3,
-    )
+#     log = (
+#         "=" * 10,
+#         " GET REQUEST ",
+#         "=" * 10,
+#         f"\nTime: {time}",
+#         f"\nIP: {real_ip}",
+#         f"\nURL: {url}",
+#         f"\nAgent: {user_agent}\n",
+#         "=" * 10 + " ",
+#         "-" * 11 + " ",
+#         "=" * 10,
+#         "\n" * 3,
+#     )
 
-    with open("Requests.log", "a") as file:
-        file.writelines("".join(log))
+#     with open("Requests.log", "a") as file:
+#         file.writelines("".join(log))
 
 
 @app.route("/")
 def index():
-    log_info()
+    # log_info()
     return render_template("en.html")
 
 
 @app.route("/lv")
 def lv():
-    log_info()
+    # log_info()
     return render_template("lv.html")
 
 
 @app.route("/ru")
 def ru():
-    log_info()
+    # log_info()
     return render_template("ru.html")
 
 
